5046453fbbfd8d1a3c1e9e8c87402c019a8bc9bc
Yiting Lei <ytlei@umd.edu>
2017-12-15 00:30:10 -0500

    PB2.3 edit odomTracker

--- include/odomTracker.hpp
+++ include/odomTracker.hpp
@@ -14,16 +14,46 @@
 #include "ball_collector_robot/GetOdom.h"
 #include "ball_collector_robot/GetOdomPretty.h"
 
-
-namespace {
-
 class odomTracker {
-public:
-	odomTracker();
-	virtual ~odomTracker();
-};
+private:
+	ros::NodeHandle n;
+	ros::ServiceServer getOdomService;
+	ros::ServiceServer getOdomPrettyService;
+	// subscriber for odometry, where are we now?
+	ros::Subscriber odomSubscriber;
+	// current location
+	geometry_msgs::Pose location;
 
-} /* namespace  */
-} /* namespace internal */
+	/**
+	 * handler for odom subscriber
+	 */
+	void handleOdom(nav_msgs::Odometry odom);
+
+public:
+	/**
+	 * Construct and initialize
+	 * @param nh the valid node handle for this node
+	 */
+	explicit OdomTracker(ros::NodeHandle nh);
+	/**
+	 * Destructor
+	 */
+	virtual ~OdomTracker();
+	/**
+	 * Starts looping
+	 */
+	void spin();
+	/**
+	 * Service for get current odom
+	 */
+	bool getOdom(ball_collector_robot::GetOdom::Request &req,
+			ball_collector_robot::GetOdom::Response &resp);
+
+	/**
+	 * Service for get current odom in more readable format
+	 */
+	bool getOdomPretty(ball_collector_robot::GetOdomPretty::Request &req,
+			ball_collector_robot::GetOdomPretty::Response &resp);
+};
 
 #endif /* SRC_ODOMTRACKER_HPP_ */
--- src/odomTracker.cpp
+++ src/odomTracker.cpp
@@ -6,18 +6,78 @@
  */
 
 #include "odomTracker.hpp"
+#include "ros/ros.h"
+#include <math.h>
 
-namespace internal {
-namespace {
+double quaternionToZAngle(const geometry_msgs::Quaternion &q) {
+	double ysqr = q.y * q.y;
 
-odomTracker::odomTracker() {
-	// TODO Auto-generated constructor stub
+	// yaw (z-axis rotation)
+	double t3 = +2.0 * (q.w * q.z + q.x * q.y);
+	double t4 = +1.0 - 2.0 * (ysqr + q.z * q.z);
+	double yaw = std::atan2(t3, t4);
 
+	return yaw * 180.0 / M_PI;
+}
+
+odomTracker::odomTracker(ros::NodeHandle nh) {
+	n = nh;
+	// Register our services with the master.
+	getOdomService = n.advertiseService("get_odom", &OdomTracker::getOdom,
+			this);
+	getOdomPrettyService = n.advertiseService("get_odom_pretty",
+			&OdomTracker::getOdomPretty, this);
+	odomSubscriber = n.subscribe("odom", 10, &OdomTracker::handleOdom, this);
 }
 
 odomTracker::~odomTracker() {
-	// TODO Auto-generated destructor stub
+	// nothing to do
 }
 
-} /* namespace  */
-} /* namespace internal */
+void odomTracker::spin() {
+	ROS_INFO_STREAM("Starting Odom Tracker ...");
+	ros::Rate loop_rate(10);
+	while (ros::ok()) {
+		// keep alive
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+}
+
+bool odomTracker::getOdom(ball_collector_robot::GetOdom::Request &req,
+		ball_collector_robot::GetOdom::Response &resp) {
+	(void) req;  // Suppress unused warning
+	resp.pose = location;
+	return true;
+}
+
+bool odomTracker::getOdomPretty(
+		ball_collector_robot::GetOdomPretty::Request &req,
+		ball_collector_robot::GetOdomPretty::Response &resp) {
+	(void) req;  // Suppress unused warning
+	resp.point = location.position;
+	resp.angleDegrees = quaternionToZAngle(location.orientation);
+	return true;
+}
+
+void odomTracker::handleOdom(nav_msgs::Odometry odom) {
+	ROS_DEBUG_STREAM(
+			"New Odom At: \n(" << odom.pose.pose.orientation.x << ", "
+					<< odom.pose.pose.orientation.y << ")");
+	location.orientation.w = odom.pose.pose.orientation.w;
+	location.orientation.x = odom.pose.pose.orientation.x;
+	location.orientation.y = odom.pose.pose.orientation.y;
+	location.orientation.z = odom.pose.pose.orientation.z;
+
+	location.position.x = odom.pose.pose.position.x;
+	location.position.y = odom.pose.pose.position.y;
+	location.position.z = odom.pose.pose.position.z;
+}
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "odom_tracker");
+	ros::NodeHandle n;
+	odomTracker odom_tracker(n);
+	odom_tracker.spin();
+	return 0;
+}
